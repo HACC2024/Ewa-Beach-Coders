0f77d037061e556ba84d7136bd24e966ad964c06
zhanebell <zhanebell05@gmail.com>
2024-11-11 19:55:20 -1000

    commit this

--- src/build_embeddings.py
+++ src/build_embeddings.py
@@ -1,5 +1,5 @@
 from embedding import EmbeddingStore, build_embedding_store
 
-scraped_data_folder = "ScrapedData"
+scraped_data_folder = "Scraped Data"
 embedding_store = EmbeddingStore()
 build_embedding_store(scraped_data_folder, embedding_store)
